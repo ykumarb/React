5d11be0cc4f7d55cc054a7dee642da22bb906fc0
Yupindra Kumar (CW) <ykumar@synaptics.com>
2024-09-14 19:33:27 +0530

    Committing the code learnt on component model in React
    CL:
        1. Components and its use.
        2. Types of components.
        3. Example of class/Function based components.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,28 @@
-function App() {
-  return (
-    <h1>Welcome to React!</h1>
-  );
+import React from 'react';
+// import {Component} from 'react';
+
+/*
+COMPONENTS:
+Components are independent and reusable bits of code.
+They serve the same purpose as JavaScript functions, 
+but work in isolation and return HTML via a render() function.
+
+Types:
+1. Class components
+2. Functional components
+*/
+
+// /* Functional components */
+// function App() {
+//   return <h1>Hello Yupindra</h1>
+// }
+
+/* Class based components */
+class App extends React.Component {
+  // To show content on the screen.
+  render() {
+    return <h1>Hello Yupindra</h1>
+  }
 }
 
 export default App;
