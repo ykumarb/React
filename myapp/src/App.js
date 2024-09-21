a4784f46e4a6354cb0be8bb422de0fe3fe26ce33
Yupindra Kumar (CW) <ykumar@synaptics.com>
2024-09-21 23:00:35 +0530

    useReducer hook in react js.
    For handling complex state logic involving multiple sub values.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -703,27 +703,76 @@ It can be used together with the useState hook to share state between deeply nes
 components more easily than with useState alone.
 */
 
-import { createContext } from 'react';
-import Greetings from './components/Greetings';
+// import { createContext } from 'react';
+// import Greetings from './components/Greetings';
 
-export const Data = createContext();
-export const Data1 = createContext();
+// export const Data = createContext();
+// export const Data1 = createContext();
 
+// function App() {
+
+//   const name = "Yupindra Kumar Balaji";
+//   const age = 25;
+
+//   return (
+//     <>
+//       <h1>useContext Hook</h1>
+//       <Data.Provider value={name}>
+//         <Data1.Provider value={age}>
+//           < Greetings />
+//         </Data1.Provider>
+//       </Data.Provider>
+//     </>
+//   );
+// };
+
+// export default App;
+
+//-------------------------useReducer Hook--------------------------------------
+/*
+  useReducer is a hook that is similar to the useState hook.
+  But it is designed for more complex state objects or state transitions that
+  involve multiple sub-values.
+  It allows us to manage state in a functional, immutable way.  
+*/
+
+import { useReducer } from 'react';
 function App() {
 
-  const name = "Yupindra Kumar Balaji";
-  const age = 25;
-
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      <h1>useContext Hook</h1>
-      <Data.Provider value={name}>
-        <Data1.Provider value={age}>
-          < Greetings />
-        </Data1.Provider>
-      </Data.Provider>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({type: "increment"})}>+</button>
+      <button onClick={() => dispatch({type: "decrement"})}>-</button>
+      <button onClick={() => dispatch({type: "reset"})}>Reset</button>
     </>
-  );
+  )
 };
 
+const initialState = {count: 0};
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case "decrement":
+    return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    case "reset":
+      return {
+        ...state,
+        count: 0,
+      };
+    default:
+      return state;
+  }
+}
 export default App;
